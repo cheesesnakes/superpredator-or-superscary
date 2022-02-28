879466b03806621c85917e11ed3219bcedf45d85
Shawn Dsouza <shawn27.dsouza@gail.com>
2022-02-28 16:52:52 +0530

    first commit, Simulating meta-analysis data

---
+++ meta-data-sim.R
@@ -0,0 +1,15 @@
+# Effect of fisheries on herbivore behavior
+
+setwd("C:\\Users\\shony\\Nextcloud\\Work\\PhD\\Thesis\\review\\Analysis")
+
+# simulating data for meta analysis
+
+## variables
+
+refs <- c("foo et al. 9999", "bar et al. 9999", "baz et al. 9999") # references
+
+fishing <- c("Present", "Absent") # presence of fishing boats
+
+effect <- c("Foraging area", "bite rate", "Giving up density") # effect on herbivore behavior
+
+  
